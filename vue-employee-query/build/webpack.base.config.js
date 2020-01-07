f99049ba48c127208229ea27ce202b210c6ba0b2
Bianwy <bianwy025@qq.com>
2020-01-07 09:25:05 +0800

    自定义封装组件。

---
+++ vue-employee-query/build/webpack.base.config.js
@@ -0,0 +1,46 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  output: {
+    filename: 'employee-query.js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.vue'],
+    alias: {
+      vue: 'vue/dist/vue.esm.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.tsx?$/i,
+        use: [{
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/]
+          }
+        }],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+}
